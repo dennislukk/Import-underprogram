be8973643621d869da2cbd66d29b92fda658236d
DennisLukk <dennislukkedal@gmail.com>
2024-11-07 10:40:49 +0100

    r4r4

---
+++ hovedprogram.py
@@ -0,0 +1,12 @@
+#Dette er hovedprogrammet
+import underprogram
+
+underprogram.funk_skrivUT("Fra hovedprogram")
+
+#importere bare funk
+from underprogram import funk_skrivUT
+funk_skrivUT("importert bare den ene funksjonen")
+
+#importere som noe annet
+from underprogram import funk_skrivUT as ut
+ut("korteste")
---
+++ underprogram.py
@@ -0,0 +1,5 @@
+#Her skal vi ha en funksjon som skriver ut
+def funk_skrivUT(var_a):
+    print(var_a)
+
+
